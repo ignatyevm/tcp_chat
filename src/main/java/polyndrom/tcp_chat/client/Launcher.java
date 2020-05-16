4b239018d568915aa97fea9f2d1ecfefd043a99e
polyndrom <polyndromdev@gmail.com>
2020-05-17 09:57:26 +1000

    Launcher

---
+++ src/main/java/polyndrom/tcp_chat/client/Launcher.java
@@ -0,0 +1,11 @@
+package polyndrom.tcp_chat.client;
+
+import java.io.IOException;
+
+public class Launcher {
+
+    public static void main(String[] args) throws IOException {
+        TcpChatMain.main(args);
+    }
+
+}
